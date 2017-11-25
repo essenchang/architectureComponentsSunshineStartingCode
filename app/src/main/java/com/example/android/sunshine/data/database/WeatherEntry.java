99e2c8f68db174352769791f3261db823964ea6a
essenchang <essenchang@gmail.com>
2017-11-25 15:33:54 +0800

    detailactivity, mvvm 完成

--- app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherEntry.java
@@ -16,10 +16,19 @@
 
 package com.example.android.sunshine.data.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+// 建立資料表名稱 - weather
+// 將 date 設定為 unique index
+@Entity(tableName = "weather", indices = {@Index(value = {"date"}, unique = true)})
 public class WeatherEntry {
 
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private int weatherIconId;
     private Date date;
@@ -31,8 +40,36 @@ public class WeatherEntry {
     private double degrees;
 
     /**
+     * Room 用的建構式
+     *
+     * @param id
+     * @param weatherIconId
+     * @param date
+     * @param min
+     * @param max
+     * @param humidity
+     * @param pressure
+     * @param wind
+     * @param degrees
+     */
+    public WeatherEntry(int id, int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
+        this.id = id;
+        this.weatherIconId = weatherIconId;
+        this.date = date;
+        this.min = min;
+        this.max = max;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.wind = wind;
+        this.degrees = degrees;
+    }
+
+    /**
      * This constructor is used by OpenWeatherJsonParser. When the network fetch has JSON data, it
      * converts this data to WeatherEntry objects using this constructor.
+     *
+     * 不是要給 Room 用的建構式要記得加上 @Ignore
+     *
      * @param weatherIconId Image id for weather
      * @param date Date of weather
      * @param min Min temperature
@@ -42,6 +79,7 @@ public class WeatherEntry {
      * @param wind Wind speed
      * @param degrees Wind direction
      */
+    @Ignore
     public WeatherEntry(int weatherIconId, Date date, double min, double max, double humidity, double pressure, double wind, double degrees) {
         this.weatherIconId = weatherIconId;
         this.date = date;
